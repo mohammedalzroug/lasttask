5a79a6ed17825e9d34b9fa98270abc39f8bb22ab
mohammed alzroug <malzroug@gmail.com>
2023-03-10 14:34:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+function sideBar() {
+    document.getElementById("SideBar1").style.display= "flex";
+    document.getElementById("SideBar1").style.width= "300px";
+    document.getElementById("SideBar1").style.flexDirection = "column"
+}
+
+
+function closNav() {
+    document.getElementById("SideBar1").style.display= "none";
+}
+
+function darkMood() {
+    document.getElementById("body").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("SideBar1").style.backgroundColor = "white";
+    document.getElementById("drakmood").style.display = "none";
+    document.getElementById("list").style.color = "white";
+    document.getElementById("sun").style.display = "block";
+    document.getElementById("sun").style.color = "white";
+    document.getElementById("h2-1").style.color = "white";
+    document.getElementById("sec7-h1").style.color = "white";
+    document.getElementById("card-31").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("card-32").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("card-33").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("card-34").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("card-35").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("card-36").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("sec5").style.backgroundColor = "gray";
+    
+}
+
+function lightMood() {
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("SideBar1").style.backgroundColor = "rgb(70, 68, 68)";
+    document.getElementById("drakmood").style.display = "block";
+    document.getElementById("list").style.color = "black";
+    document.getElementById("sun").style.display = "none";
+    document.getElementById("h2-1").style.color = "black";
+    document.getElementById("sec7-h1").style.color = "white";
+    document.getElementById("card-31").style.backgroundColor = "white";
+    document.getElementById("card-32").style.backgroundColor = "white";
+    document.getElementById("card-33").style.backgroundColor = "white";
+    document.getElementById("card-34").style.backgroundColor = "white";
+    document.getElementById("card-35").style.backgroundColor = "white";
+    document.getElementById("card-36").style.backgroundColor = "white";
+    document.getElementById("sec5").style.backgroundColor = "#ddaf7b";
+    
+    
+}
+
+    
+
+   
+    
+   
+    
+   
+
+
+
+
+
+
